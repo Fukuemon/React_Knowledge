be4a9a9365ccb0090de44983f1632c691c313f56
Fukuemon <Fukuemony029@gmail.com>
2023-08-25 20:18:02 +0900

    🎨 ESLintの設定 #7

--- 05_Linter/eslint/src/main.tsx
+++ 05_Linter/eslint/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+const rootElement = document.getElementById('root');
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+if (rootElement === null) {
+  throw new Error('root element not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
