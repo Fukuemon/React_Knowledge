c4a3088078b35414e28bf1b39066e2797561c787
Fukuemon <Fukuemony029@gmail.com>
2023-08-26 22:58:17 +0900

    ✨ Headerの作成 #9
    Flexで要素を横に並べる
    alignとjustifyで要素を均等に配置
    プロパティ={{}}でレスポンシブ設定
    _hover~{{}}で擬似要素指定

--- 999_Practice/react-chakra/src/components/layout/Header.tsx
+++ 999_Practice/react-chakra/src/components/layout/Header.tsx
@@ -1,6 +1,41 @@
 import React from "react";
 import { memo, FC } from "react";
+import { Flex, Heading, Link, Box } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: FC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "red" }}></div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      align="center"
+      color="gray.50"
+      padding={{ base: 3, md: 5 }} // base: 320px, md: 768px
+      justify="space-between"
+    >
+      <Flex
+        align="center"
+        as="a"
+        mr={8}
+        _hover={{ color: "blue.500", cursor: "pointer" }}
+      >
+        <Heading as="h1" fontSize={{ base: "mg", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
+      <Flex
+        align="center"
+        fontSize={"sm"}
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+        _hover={{ color: "blue.500", cursor: "pointer" }}
+      >
+        <Box pr={4}>
+          <Link>ユーザー一覧</Link>
+        </Box>
+        <Link>設定</Link>
+      </Flex>
+      <HamburgerIcon boxSize={8} />
+    </Flex>
+  );
 });
