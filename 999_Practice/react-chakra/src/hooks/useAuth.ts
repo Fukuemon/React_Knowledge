cea561a22242a912659f3f8fc9758a4560e84c48
Fukuemon <Fukuemony029@gmail.com>
2023-08-27 12:55:36 +0900

    ⚡️ fetchとaxiosの書き換え #9

--- 999_Practice/react-chakra/src/hooks/useAuth.ts
+++ 999_Practice/react-chakra/src/hooks/useAuth.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useCallback, useState } from "react";
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
@@ -7,21 +6,46 @@ export const useAuth = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
-  const login = useCallback((id: string) => {
-    setLoading(true);
-    axios
-      .get<User>(`https:/jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        if (res.data) {
+  const login = useCallback(
+    async (id: string) => {
+      setLoading(true);
+      try {
+        // try catchでエラーをキャッチ
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        const user = (await res.json()) as User;
+        if (user) {
           navigate("/home");
-          [navigate];
         } else {
-          alert("ユーザーが見つかりません");
+          alert("ユーザーが見つかりません!");
         }
-      })
-      .catch(() => alert("ログインできませんでした"))
-      .finally(() => setLoading(false));
-  }, []);
+      } catch (err) {
+        // エラーが発生した場合
+        alert("ログインできませんでした!");
+      } finally {
+        setLoading(false); // finallyは成功しても失敗しても実行される
+      }
+    },
+    [navigate]
+  );
   return { login, loading };
+
+  //   const login = useCallback((id: string) => {
+  //     setLoading(true);
+  //     axios
+  //       .get<User>(`https:/jsonplaceholder.typicode.com/users/${id}`)
+  //       .then((res) => {
+  //         console.log(res.data);
+  //         if (res.data) {
+  //           navigate("/home");
+  //           [navigate];
+  //         } else {
+  //           alert("ユーザーが見つかりません");
+  //         }
+  //       })
+  //       .catch(() => alert("ログインできませんでした"))
+  //       .finally(() => setLoading(false));
+  //   }, []);
+  //   return { login, loading };
 };
