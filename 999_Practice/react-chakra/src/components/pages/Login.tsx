d48b8199c83ff7e196e6fa3983884b748244b09b
Fukuemon <Fukuemony029@gmail.com>
2023-08-27 11:16:31 +0900

    🎨 コンポーネントの分割 #9
    PrimaryButtonでコンポーネントを分割
    propsとして子要素を引き取る

--- 999_Practice/react-chakra/src/components/pages/Login.tsx
+++ 999_Practice/react-chakra/src/components/pages/Login.tsx
@@ -1,13 +1,6 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { memo, FC } from "react";
+import { PrimaryButton } from "../atoms/Button/PrimaryButton";
 
 export const Login: FC = memo(() => {
   return (
@@ -19,9 +12,7 @@ export const Login: FC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton children={"ログイン"} />
         </Stack>
       </Box>
     </Flex>
