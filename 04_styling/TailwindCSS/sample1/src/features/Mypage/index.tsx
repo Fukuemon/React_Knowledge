13550e77295d42a2c118a005326272df8deeb24d
Fukuemon <Fukuemony029@gmail.com>
2023-09-13 15:39:12 +0900

    💄 コンテンツ部分の作成#2

--- 04_styling/TailwindCSS/sample1/src/features/Mypage/index.tsx
+++ 04_styling/TailwindCSS/sample1/src/features/Mypage/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../../components/layout/Navbar';
 import ProfileHeader from '../../components/UserProfile/Header';
 import MyPageHeader from '../../components/Mypage/Header';
+import MyContents from '../../components/Mypage/Contents';
 
 const MyPage = () => {
   return (
     <>
       <Header />
       <MyPageHeader />
+      <MyContents />
     </>
   ); // <Header />を返す
 };
